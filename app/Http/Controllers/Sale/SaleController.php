be6074df36ebee7454afe4875770a81615eabe23
Gowthyn <gowthamnandakumar.aha@gmail.com>
2022-06-11 11:38:57 +0530

    done

--- app/Http/Controllers/Sale/SaleController.php
+++ app/Http/Controllers/Sale/SaleController.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Sale;
 
+use App\Http\Controllers\Controller;
 use App\Sale;
 use Illuminate\Http\Request;
+
 
 class SaleController extends Controller
 {
--- resources/views/Sale/index.blade.php
+++ resources/views/Sale/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['page' => 'List of Purchase', 'pageSlug' => 'purchase', 'section' => 'PurchaseMaster'])
+{{-- @extends('layouts.app', ['page' => 'List of Sale', 'pageSlug' => 'salle', 'section' => 'SaleMaster'])
 
 @section('content')
 <style>
@@ -7,7 +7,17 @@
         width: 33%;
     }
 
-    .middle {
+    .middlea {
+        float: left;
+        width: 33%;
+    }
+
+    .middleb {
+        float: left;
+        width: 33%;
+    }
+
+    .middlec {
         float: left;
         width: 33%;
     }
@@ -26,36 +36,62 @@
         border: 2px solid blue !important;
     }
 </style>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" ></script> 
-      <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
-      <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script> 
-      
 <div class="card">
     <div class="card-header text-center font-weight-bold text-white" style="background-color: #5e72e4;">
-        Purchase
+        ManageStock
     </div>
     <div class="card-body">
 
         <div class="row">
-            <div class="first col-md-3 col-sm-3 col-md-pull-3">
-                <div class="h-100 d-flex align-items-center justify-content-center ">
+            <div class="first col-md-2 col-sm-2 col-md-pull-2">
+
+                <div class="h-100 d-flex align-items-center justify-content-center">
                     <div class="SplitData activeSplitterDiv" data-value="AllData" style=" height: auto;width:130px;background-color:#265362;border-radius: 10px;
                 font-size: 20px;text-align: center;">
-                        <img src="assets/img/hotel-supplier.png" alt="" style="width: 50px;margin-top:10px;">
+                        <img src="assets/img/hotel-supplier.png" alt="" style=" width: 50px;margin-top:10px;">
                         <div class="tee" style="font-size: 20px;color: #fff;">Total</div>
                     </div>
                 </div>
+
             </div>
-            <div class="middle col-md-3 col-md-push-3 col-sm-3">
+
+            <div class="middlea col-md-2 col-md-push-2 col-sm-2">
                 <div class="h-100 d-flex align-items-center justify-content-center">
-                    <div class="SplitData" data-value="activeData" style=" height: auto;width:130px;background-color:#265362;border-radius: 10px;
-                font-size: 20px;text-align: center; ">
+                    <div class="SplitData" data-value="Availability" class="activeSplitterDiv" style=" height: auto;width:130px;background-color:#265362;border-radius: 10px;
+                font-size: 20px;text-align: center;">
                         <img src="assets/img/active.png" alt="" style="width: 50px;margin-top:10px;">
-                        <div class="tee" style="font-size: 20px;color: #fff;">Active</div>
+                        <div class="tee" style="font-size: 20px;color: #fff;">Available</div>
+                        <div style=text-decoration: underline;></div>
                     </div>
                 </div>
+
             </div>
-            <div class="last col-md-3 col-sm-3 ">
+
+            <div class="middleb col-md-2 col-md-push-2 col-sm-2">
+                <div class="h-100 d-flex align-items-center justify-content-center">
+                    <div class="SplitData" data-value="Demand" class="activeSplitterDiv" style=" height: auto;width:130px;background-color:#265362;border-radius: 10px;
+                font-size: 20px;text-align: center;">
+                        <img src="assets/img/active.png" alt="" style="width: 50px;margin-top:10px;">
+                        <div class="tee" style="font-size: 20px;color: #fff;">Demand</div>
+                        <div style=text-decoration: underline;></div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="middle col-md-2 col-md-push-2 col-sm-2">
+                <div class="h-100 d-flex align-items-center justify-content-center">
+                    <div class="SplitData" data-value="activeData" style=" height: auto;width:130px;background-color:#265362;border-radius: 10px;
+                font-size: 20px;text-align: center;">
+                        <img src="assets/img/active.png" alt="" style="width: 50px;margin-top:10px;">
+                        <div class="tee" style="font-size: 20px;color: #fff;">Active</div>
+                        <div style=text-decoration: underline;></div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="last col-md-2 col-sm-2">
                 <div class="h-100 d-flex align-items-center justify-content-center">
                     <div class="SplitData" data-value="inActiveData" class="activeSplitterDiv" style=" height: auto;width:130px;background-color:#265362;border-radius: 10px;
                 font-size: 20px;text-align: center;">
@@ -63,6 +99,7 @@
                         <div class="tee" style="font-size: 20px;color: #fff;">In-Active</div>
                     </div>
                 </div>
+
             </div>
 
         </div>
@@ -78,9 +115,7 @@
                         <h4 class="card-title"></h4>
                         <input type="text" id="search" placeholder="Type to search" autocomplete="off">
                     </div>
-                    <div class="col-4 text-right">
-                        <a href="{{ route('purchase.create') }}" class="btn btn-sm btn-primary"> Add Purchase</a>
-                    </div>
+
                 </div>
             </div>
             <div class="card-body">
@@ -90,40 +125,30 @@
                     <table class="table tablesorter " id="">
                         <thead class=" text-primary">
                             <th scope="col">#</th>
-                            <th scope="col">Supplier ID</th>
-                            <th scope="col">Product ID</th>
-                            <th scope="col">Quantity</th>
-                            <th scope="col">Barcode</th>
-                            <th class="status" scope="col">Status</th>
-                            <th scope="col">Actions</th>
+                            <th scope="col">Product Name</th>
+                            <th scope="col">Category</th>
+                            <th scope="col">Brand</th>
+                            <th scope="col">Currnet Quantity</th>
+                            <th scope="col">Availablitity</th>
+                            <th scope="col">Order</th>
 
                         </thead>
-                        <tbody>
-                            @foreach($models as $model)
+                        {{-- <tbody>
+                            @foreach ($models as $model)
                             <tr>
                                 <td>{{$loop->iteration}}</td>
-                                <td>{{ $model->supplierId }}</td>
-                                <td>{{ $model->item_id }}</td>
-                                <td>{{ $model->quantity }}</td>
-                                <td>{{ $model->barcode }}</td>
-                                <td></label>
-                                 </td>
+                                <td>{{ $model->product_name }}</td>
+                                <td>{{ $model->categoryName }}</td>
+                                <td>{{ $model->brandName }}</td>
+                                <td>{{ $model->stock }}{{ $model->unitName }}</td>
+                                <td>@if($model->limit>=$model->stock)<span class="badge badge-success">Suffiecient</span>
+                                    @else<span class="badge badge-danger">In-Suffiecient</span>@endif</td>
+                                <td><a href=""><span class="badge badge-primary">Order</span></a></td>
 
-                                <td class="td-actions">
-                                    <a href="{{ route('purchase.edit', $model->id) }}" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Edit Product">
-                                        <i class="tim-icons icon-pencil"></i>
-                                    </a>
-                                    <form action="{{ url(' purchase/{purchase}', $model->id) }}" method="post" class="d-inline">
-                                        @csrf
-                                        @method('delete')
-                                        <button type="button" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Delete Product" onclick="confirm('Are you sure you want to remove this product? The records that contain it will continue to exist.') ? this.parentElement.submit() : ''">
-                                            <i class="tim-icons icon-simple-remove"></i>
-                                        </button>
-                                    </form>
-                                </td>
+
                             </tr>
                             @endforeach
-                        </tbody>
+                        </tbody> --}}
                     </table>
                 </div>
             </div>
@@ -136,72 +161,82 @@
     </div>
 </div>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script>
-        $(document).ready(function() {
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    $(document).ready(function() {
 
-        });
-        var $rows = $('.table tbody tr');
-        $('#search').keyup(function() {
-            var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+    });
 
-            $rows.show().filter(function() {
-                var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-                return !~text.indexOf(val);
-            }).hide();
-        });
+    $("#search").keyup(function() {
 
-        var type = null;
-        $('.SplitData').click(function(e) {
-            $("body").find('.SplitData').removeClass('activeSplitterDiv');
+        var value = this.value.toLowerCase().trim();
 
-            $(this).addClass('activeSplitterDiv');
-            type = $(this).attr('data-value');
-            splitData(type);
-        });
-
-        function splitData(type) {
-
-            $.ajax({
-                url: "{{ route('getPurchaseSplitedData') }}",
-                type: "post",
-                data: type,
-                data: {
-                    _token: '{{ csrf_token() }}',
-                    type: type,
-
-                },
-                success: function(response) {
-
-                    var Result = response.data;
-                    $(".table tbody").html("");
-                    $.each(Result, function(key, value) {
-                        var editurl = '{{ route('purchase.edit', ':id') }}';
-                        editurl = editurl.replace(':id', value.id);
-
-                        var deleteurl = '{{ route('purchase.destroy', ':id') }}';
-                        deleteurl = deleteurl.replace(':id', value.id);
-
-                        var row = `<tr role="row" class="odd"><td>` + (key + 1) + `</td>
-                            <td>` + value.supplierId + `</td>
-                            <td>` + value.item_id + `</td>
-                            <td>` + value.quantity + `</td>
-                            <td>` + value.barcode + `</td>
-                            <td class="td-actions"><a href ="` + editurl +
-                            `" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Edit"> <i class="tim-icons icon-pencil"></i></a><form id="deleteStudentForm" action="` +
-                            deleteurl + `" method="post" class="d-inline"> @csrf @method('DELETE') <button type="submit" class="btn btn-link" data-toggle="tooltip"
-                                            data-placement="bottom" title="Delete Product" onclick="return confirm('Are you sure?')">
-                                            <i class="tim-icons icon-simple-remove"></i>
-                                        </button></form></td></tr>`;
-                        //$('.table tbody').append('<tr> <td>' + (key + 1) + '</td><td>' + value.brand_name + '</td><td><a href ="" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Edit"> <i class="tim-icons icon-pencil"></i></a><form action="" method="post" class="d-inline">@csrf @method('delete')<button type="button" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Delete Product" onclick="confirm("Are you sure you want to remove this product? The records that contain it will continue to exist.") ? this.parentElement.submit() : ' + " " + '"> <i class="tim-icons icon-simple-remove"></i></button></form></td></tr>');
-                        $('.table tbody').append(row);
-                    })
-                    // You will get response from your PHP page (what you echo or print)
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    console.log(textStatus, errorThrown);
-                }
+        $("table tr").each(function(index) {
+            if (!index) return;
+            $(this).find("td").each(function() {
+                var id = $(this).text().toLowerCase().trim();
+                var not_found = (id.indexOf(value) == -1);
+                $(this).closest('tr').toggle(!not_found);
+                return not_found;
             });
-        }
-    </script>
-@endsection
+        });
+    });
+
+    var type = null;
+    $('.SplitData').click(function(e) {
+        $('#search').val("");
+        $("body").find('.SplitData').removeClass('activeSplitterDiv');
+
+        $(this).addClass('activeSplitterDiv');
+        type = $(this).attr('data-value');
+        splitData(type);
+    });
+
+    function splitData(type) {
+        console.log(type);
+        $.ajax({
+            url: "{{ route('getManageStockSplitedData') }}",
+            type: "post",
+            data: type,
+            data: {
+                _token: '{{ csrf_token() }}',
+                type: type,
+
+            },
+            success: function(response) {
+
+                var Result = response.data;
+                console.log(Result);
+
+                $(".table tbody tr ").html("");
+                $.each(Result, function(key, value) {
+                    var editurl = '{{ route('
+                    brand.edit ', ': id ') }}';
+                    editurl = editurl.replace(':id', value.id);
+
+                    var deleteurl = '{{ route('
+                    brand.destroy ', ': id ') }}';
+                    deleteurl = deleteurl.replace(':id', value.id);
+                    var stock = value.stock;
+                    var limit = value.limit;
+                    var badge = "";
+                    if (limit >= stock) {
+                        badge = '<span class="badge badge-danger">In-Suffiecient</span>';
+                    } else {
+                        badge = '<span class="badge badge-success">Suffiecient</span>';
+                    }
+                    console.log(badge);
+                    var row = `<tr><td>` + (key + 1) + `</td><td>` + value
+                        .product_name + `</td><td>` + value.categoryName + `</td><td>` + value.brandName + `</td><td>` + value.stock + " " + value.unitName + `</td><td>` + badge + `</td><td><a href=""><span class="badge badge-primary">Order</span></a></td></tr>`;
+                    //$('.table tbody').append('<tr> <td>' + (key + 1) + '</td><td>' + value.brand_name + '</td><td><a href ="" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Edit"> <i class="tim-icons icon-pencil"></i></a><form action="" method="post" class="d-inline">@csrf @method('delete')<button type="button" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Delete Product" onclick="confirm("Are you sure you want to remove this product? The records that contain it will continue to exist.") ? this.parentElement.submit() : ' + " " + '"> <i class="tim-icons icon-simple-remove"></i></button></form></td></tr>');
+                    $('.table tbody').append(row);
+                })
+                // You will get response from your PHP page (what you echo or print)
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+            }
+        });
+    }
+</script>
+@endsection --}}
