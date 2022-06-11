ec296e005a6945aa8ffb14b98e50464ea43182dd
Gowthyn <gowthamnandakumar.aha@gmail.com>
2022-06-11 11:20:16 +0530

    done

---
+++ app/Http/Controllers/Sale/ReturnSaleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ReturnSale;
+use Illuminate\Http\Request;
+
+class ReturnSaleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ReturnSale  $returnSale
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ReturnSale $returnSale)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ReturnSale  $returnSale
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ReturnSale $returnSale)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ReturnSale  $returnSale
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ReturnSale $returnSale)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ReturnSale  $returnSale
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ReturnSale $returnSale)
+    {
+        //
+    }
+}
