3e24e48e6f68de2db9afb11719f2766dc637107a
Gowthyn <gowthamnandakumar.aha@gmail.com>
2022-06-23 14:55:42 +0530

    removed the addinv by Gowtham

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -328,12 +328,12 @@
                 </div>
             </li> -->
 
-            <li @if ($pageSlug=='addinv' ) class="active " @endif>
+            {{-- <li @if ($pageSlug=='addinv' ) class="active " @endif>
                 <a href="{{ route('addinv.index') }}">
                     <i class="tim-icons icon-single-02"></i>
                     <p>Add Inv</p>
                 </a>
-            </li>
+            </li> --}}
 
             <li>
                 <a data-toggle="collapse" href="#users" {{ $section == 'users' ? 'aria-expanded=true' : '' }}>
