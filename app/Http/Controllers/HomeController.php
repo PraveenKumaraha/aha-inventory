063b7b860f0ec0fbdeaea130e5478a759190aa58
Dhanatrichy <dhanraj@propelsoft.in>
2022-06-28 10:49:45 +0530

    Added Dashboard code by dhana

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Sale;
 use Carbon\Carbon;
 use App\SoldProduct;
@@ -17,27 +18,7 @@ class HomeController extends Controller
 
     public function index()
     {
-        $monthlyBalanceByMethod = $this->getMethodBalance()->get('monthlyBalanceByMethod');
-        $monthlyBalance = $this->getMethodBalance()->get('monthlyBalance');
-
-        $anualsales = $this->getAnnualSales();
-        $anualclients = $this->getAnnualClients();
-        $anualproducts = $this->getAnnualProducts();
-        
-        return view('dashboard', [
-            'monthlybalance'            => $monthlyBalance,
-            'monthlybalancebymethod'    => $monthlyBalanceByMethod,
-            'lasttransactions'          => Transaction::latest()->limit(20)->get(),
-            'unfinishedsales'           => Sale::where('finalized_at', null)->get(),
-            'anualsales'                => $anualsales,
-            'anualclients'              => $anualclients,
-            'anualproducts'             => $anualproducts,
-            'lastmonths'                => array_reverse($this->getMonthlyTransactions()->get('lastmonths')),
-            'lastincomes'               => $this->getMonthlyTransactions()->get('lastincomes'),
-            'lastexpenses'              => $this->getMonthlyTransactions()->get('lastexpenses'),
-            'semesterexpenses'          => $this->getMonthlyTransactions()->get('semesterexpenses'),
-            'semesterincomes'           => $this->getMonthlyTransactions()->get('semesterincomes')
-        ]);
+        return view('dashboard');
     }
 
     public function getMethodBalance()
@@ -57,7 +38,7 @@ class HomeController extends Controller
     public function getAnnualSales()
     {
         $sales = [];
-        foreach(range(1, 12) as $i) {
+        foreach (range(1, 12) as $i) {
             $monthlySalesCount = Sale::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', $i)->count();
 
             array_push($sales, $monthlySalesCount);
@@ -68,7 +49,7 @@ class HomeController extends Controller
     public function getAnnualClients()
     {
         $clients = [];
-        foreach(range(1, 12) as $i) {
+        foreach (range(1, 12) as $i) {
             $monthclients = Sale::selectRaw('count(distinct client_id) as total')
                 ->whereYear('created_at', Carbon::now()->year)
                 ->whereMonth('created_at', $i)
@@ -82,11 +63,11 @@ class HomeController extends Controller
     public function getAnnualProducts()
     {
         $products = [];
-        foreach(range(1, 12) as $i) { 
+        foreach (range(1, 12) as $i) {
             $monthproducts = SoldProduct::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', $i)->sum('qty');
 
             array_push($products, $monthproducts);
-        }        
+        }
         return "[" . implode(',', $products) . "]";
     }
 
@@ -109,7 +90,7 @@ class HomeController extends Controller
                 ->sum('amount');
 
             $semesterincomes += $incomes;
-            $lastincomes = round($incomes).','.$lastincomes;
+            $lastincomes = round($incomes) . ',' . $lastincomes;
 
             $expenses = abs(Transaction::whereIn('type', ['expense', 'payment'])
                 ->whereYear('created_at', $actualmonth->year)
@@ -117,13 +98,13 @@ class HomeController extends Controller
                 ->sum('amount'));
 
             $semesterexpenses += $expenses;
-            $lastexpenses = round($expenses).','.$lastexpenses;
+            $lastexpenses = round($expenses) . ',' . $lastexpenses;
 
             $actualmonth->subMonth(1);
         }
 
-        $lastincomes = '['.$lastincomes.']';
-        $lastexpenses = '['.$lastexpenses.']';
+        $lastincomes = '[' . $lastincomes . ']';
+        $lastexpenses = '[' . $lastexpenses . ']';
 
         return collect(compact('lastmonths', 'lastincomes', 'lastexpenses', 'semesterincomes', 'semesterexpenses'));
     }
