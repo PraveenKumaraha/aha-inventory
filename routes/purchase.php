c5602fbf972162a950bea75ebe0bf0cdb03851dc
PraveenKumaraha <praveenkumarp.aha@gmail.com>
2022-06-29 17:47:11 +0530

    by praveen

--- routes/purchase.php
+++ routes/purchase.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('InvPurchase', 'Purchase\InvPurchaseController');
 Route::post('/getItemData', 'Purchase\InvPurchaseController@getItemData')->name('getItemData');
+
+
+
+?>
